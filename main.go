c7467b8cbe46b262e20914dae77b89608e44fc74
jefersonla <oi_je@hotmail.com>
2022-04-15 11:16:12 -0300

    Add root route

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ func main() {
 	router := gin.Default()
 	router.SetTrustedProxies([]string{"127.0.0.1", "localhost"})
 
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
