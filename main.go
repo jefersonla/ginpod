5abdabf5675c3e3dbbd3b53946d684fa380c76e1
Jeferson Lima <oi_je@hotmail.com>
2022-06-09 11:41:48 +0000

    Add cloud function example

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+// HelloHTTP is an HTTP Cloud Function with a request parameter.
+// Copied from https://cloud.google.com/functions/docs/create-deploy-http-go
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Name string `json:"name"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Hello, World!")
+		return
+	}
+
+	if d.Name == "" {
+		fmt.Fprint(w, "Hello, World!")
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+}
 
 func main() {
 	router := gin.Default()
@@ -18,6 +43,10 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	router.GET("/hello", func(c *gin.Context) {
+		HelloHTTP(c.Writer, c.Request)
 	})
 
 	router.Run(":8080")
